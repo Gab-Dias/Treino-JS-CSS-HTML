00d739c961ec3a237a7df4fc16f4ee435103faa9
Gabriel Dias <125761843+Gab-Dias@users.noreply.github.com>
2023-06-18 13:40:00 -0300

    Add files via upload
    Atualização de lógica do calculo

--- Calculo/calculos.js
+++ Calculo/calculos.js
@@ -1,29 +1,59 @@
 function calcular() {
-    var data = new Date()
-    var anoAtual = data.getFullYear()
-    var mesAtual = data.getMonth()
-    var diaAtual = data.getDay()
+    let date = new Date()
+    let y2 = date.getFullYear()
+    let m2 = 1 + date.getMonth()
+    let d2 = date.getDay()
+    let month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-    var dia = document.getElementById("i-dia")
-    var mes = document.getElementById("i-mes")
-    var ano = document.getElementById("i-ano")
+    var d1 = document.getElementById("i-dia")
+    var m1 = document.getElementById("i-mes")
+    var y1 = document.getElementById("i-ano")
 
-    dia = Number(dia.value)
-    mes = Number(mes.value)
-    ano = Number(ano.value)
+    d1 = Number(d1.value)
+    m1 = Number(m1.value)
+    y1 = Number(y1.value)
     
     var resAno = document.getElementById("r-ano")
     var resMes = document.getElementById("r-mes")
     var resDia = document.getElementById("r-dia")   
-   
-    var calculoAno = anoAtual - ano
-    resAno.innerText = `${calculoAno}`
 
-    var calculoMes = mesAtual - mes
-     resMes.innerHTML = `${calculoMes}`
+    if (d1 > d2) {
+        d2 = d2 + month [m2 - 1]
+        m2 = m2 - 1
+    }
+    if (m1 > m2){   
+        m2 = m2 + 12
+        y2= y2 - 1
+       }
 
-    var calculoDia = dia - diaAtual
-     resDia.innerHTML = `${calculoDia}`
+    var d = d2 - d1
+    var m = m2 - m1
+    var y = y2 - y1
+
+    resAno.innerText = `${y}`
+    resMes.innerHTML = `${m}`
+    resDia.innerHTML = `${d}`
+
+
+
+    /* if (dia == 0 || dia > 31 ){
+    //     window.alert("Digite um dia entre 1 a 31")
+    // } else {
+    // var calculoAno = anoAtual - ano 
+    // resAno.innerText = `${calculoAno}` }
+
+    // if (mes == 0 || mes > 12) {
+    //     window.alert("Digite um mês entre 1 a 12")
+    // } else {
+    // var calculoMes = mesAtual - mes
+    //  resMes.innerHTML = `${calculoMes}` 
+    //         }
+        
+    // if (ano == 0 ){
+    //     window.alert("Digite um ano, exemplo (1999)")
+    // } else {
+    // var calculoDia = diaAtual - dia 
+    //  resDia.innerHTML = `${calculoDia}`} */
 
 
 }
