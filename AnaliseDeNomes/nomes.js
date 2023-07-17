dbf59d8e6e32092e37a56b01e4798c906312ec9e
Gabriel Dias <125761843+Gab-Dias@users.noreply.github.com>
2023-07-17 20:29:03 -0300

    Add files via upload

--- AnaliseDeNomes/nomes.js
+++ AnaliseDeNomes/nomes.js
@@ -1,8 +1,25 @@
-function analisarNome(){
+function analisar(){
 
-var nomes = document.getElementById("inome")
-var imagemMexendo = document.getElementById("icone")
+    let nome = document.getElementById('inome')
+
+    nome = nome.value
 
 
+    // if (nome == 'daniel' || nome == "Daniel") {
+    //     alert('Ele é gay')
+    // } else {alert('é gente boa')}
+
+    switch (nome) {
+        case "daniel":
+            alert('Ele é gay')
+            break
+
+        case 'gabriel':
+            alert('tem pau grande')
+            break
+
+        case 'Yasmin':
+            alert('Palmerense gata')
+    }
 
 }
