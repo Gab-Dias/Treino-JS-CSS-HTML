baed1ab78a69a4bdc3a1661220e3ad41811f8bf4
Gabriel Dias <125761843+Gab-Dias@users.noreply.github.com>
2023-07-24 17:43:46 -0300

    Add files via upload

--- AnaliseDeNomes/nomes.js
+++ AnaliseDeNomes/nomes.js
@@ -11,15 +11,38 @@ function analisar(){
 
     switch (nome) {
         case "daniel":
+        case "Daniel":
             alert('Ele é gay')
             break
 
         case 'gabriel':
+        case 'Gabriel':
             alert('tem pau grande')
             break
 
         case 'Yasmin':
+        case 'yasmin':
             alert('Palmerense gata')
+            break
+
+        case 'Luisa':
+        case 'luisa':
+            alert('tem pau grande')
+            break   
+        
+        case 'jose':
+        case 'Jose':
+            alert('Figado de aço')
+            break
+
+        case 'Daiane':
+        case 'daiane':
+            alert('Futura esposa de Gabriel')
+            break
+
+        case 'Guilherme':
+        case 'guilherme':
+            alert('Gosta de segurar um pau')
     }
 
 }
